bca1a2ea407a3d7cacc8ccc660997abf74f73163
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-09 23:15:45 +0300

    setup project

---
+++ MixColors/MixColors/ViewControllers/MixColorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MixColorViewController.swift
+//  MixColors
+//
+//  Created by Nikolai Maksimov on 09.02.2024.
+//
+
+import UIKit
+
+class MixColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
